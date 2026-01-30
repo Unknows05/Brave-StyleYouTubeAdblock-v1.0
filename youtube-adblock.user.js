9ae2d229d8fead76b59ac0fc38c0e89d7a5bffe8
newbie_IT <riski4949@gmail.com>
2026-01-30 08:16:55 +0700

    youtube-adblock.user.js

--- youtube-adblock.user.js
+++ youtube-adblock.user.js
@@ -7,8 +7,8 @@
 // @match        https://www.youtube.com/*
 // @match        https://m.youtube.com/*
 // @icon         https://brave.com/static-assets/images/brave-favicon.png
-// @updateURL    https://raw.githubusercontent.com/Unknows05/youtube-adblock/blob/main/youtube-adblock.user.js
-// @downloadURL  https://raw.githubusercontent.com/Unknows05/youtube-adblock/blob/main/youtube-adblock.user.js
+// @updateURL    https://github.com/Unknows05/youtube-adblock/blob/main/youtube-adblock.user.js
+// @downloadURL  https://github.com/Unknows05/youtube-adblock/blob/main/youtube-adblock.user.js
 // @grant        none
 // @run-at       document-start
 // @noframes
